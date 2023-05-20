2a036abd286acaff07c3d0db4d167253b3fced0c
simhachalam <simhachalam.amara@gmail.com>
2023-05-20 16:13:35 +0530

    added first project.

---
+++ src/main/java/com/argosinfo/argosinfosystem/ArgosinfosystemApplication.java
@@ -0,0 +1,13 @@
+package com.argosinfo.argosinfosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgosinfosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgosinfosystemApplication.class, args);
+	}
+
+}
